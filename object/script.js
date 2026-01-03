07194711a33dec4914729716088b7cc349dd6004
taqiyyhdh <taqiyyhdh@gmail.com>
2026-01-03 21:11:25 +0700

    object prototype

--- object/script.js
+++ object/script.js
@@ -13,32 +13,53 @@
 
 // 2. Function declaration
 
-const methodMahasiswa = {
-  makan: function (porsi) {
-    this.energi += porsi;
-    console.log(`halo ${this.nama}, Selamat Makan`);
-  },
+// const methodMahasiswa = {
+//   makan: function (porsi) {
+//     this.energi += porsi;
+//     console.log(`halo ${this.nama}, Selamat Makan`);
+//   },
 
-  main: function (jam) {
-    this.energi -= jam;
-    console.log(`halo ${this.nama}, selamat bermain`);
-  },
+//   main: function (jam) {
+//     this.energi -= jam;
+//     console.log(`halo ${this.nama}, selamat bermain`);
+//   },
 
-  tidur: function (jam) {
-    this.energi += jam * 2;
-    console.log(`halo ${this.nama}, selamat tidur`);
-  },
-};
+//   tidur: function (jam) {
+//     this.energi += jam * 2;
+//     console.log(`halo ${this.nama}, selamat tidur`);
+//   },
+// };
+
+// function Mahasiswa(nama, energi) {
+//   let mahasiswa = object.create(methodMahasiswa);
+//   mahasiswa.nama = nama;
+//   mahasiswa.energi = energi;
+
+//   return mahasiswa;
+// }
+//
 
 function Mahasiswa(nama, energi) {
-  let mahasiswa = object.create(methodMahasiswa);
-  mahasiswa.nama = nama;
-  mahasiswa.energi = energi;
-
-  return mahasiswa;
+  this.nama = nama;
+  this.energi = energi;
 }
-let taqi = Mahasiswa("taqi", 10);
-let jilan = Mahasiswa("jilan", 20);
+
+Mahasiswa.prototype.makan = function (porsi) {
+  this.energi += porsi;
+  return `Halo ${this.nama}, selamat makan!`;
+};
+
+Mahasiswa.prototype.main = function (jam) {
+  this.energi -= jam;
+  return `Halo ${this.nama}, selamat main!`;
+};
+
+Mahasiswa.prototype.tidur = function (jam) {
+  this.energi += jam * 2;
+  return `Halo ${this.nama}, selamat tidur!`;
+};
+
+let taqi = new Mahasiswa("Taqi", 10);
 
 // 3. Constructor function
 // function Mahasiswa(nama, energi) {
