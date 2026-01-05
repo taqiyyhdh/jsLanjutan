3f259116cd2163c6d4093d6df26d502d76f25382
taqiyyhdh <taqiyyhdh@gmail.com>
2026-01-05 20:26:04 +0700

    lanjutan arrow function

--- arrow-function/script.js
+++ arrow-function/script.js
@@ -18,7 +18,7 @@
 // const tampilNama = () => `Hello World!`;
 // console.log(tampilNama());
 
-let mahasiswa = ["Taqiyyah Adha", "Taqiyyah", "Taqi"];
+// let mahasiswa = ["Taqiyyah Adha", "Taqiyyah", "Taqi"];
 // kalo cara biasa
 // let jumlahHuruf = mahasiswa.map(function (nama) {
 //   return nama.length;
@@ -30,8 +30,55 @@ let mahasiswa = ["Taqiyyah Adha", "Taqiyyah", "Taqi"];
 // console.log(jumlahHuruf);
 
 //kalo mengembalikan dalam bentuk object bukan array
-let jumlahHuruf = mahasiswa.map((nama) => ({
-  nama: nama,
-  jumHuruf: nama.length,
-}));
-console.table(jumlahHuruf);
+// let jumlahHuruf = mahasiswa.map((nama) => ({
+//   nama: nama,
+//   jumHuruf: nama.length,
+// }));
+// console.table(jumlahHuruf);
+
+// KONSEP THIS PADA ARROW FUNCTION
+
+// CONSTRUCTOR FUNCTION
+
+// const Mahasiswa = function () {
+//   this.nama = "Taqiyyah";
+//   this.umur = "22";
+//   this.sayHello = function () {
+//     console.log(`Halo, nama saya ${this.nama}, dsn saya ${this.umur} tahun.`);
+//   };
+// };
+// const taqi = new Mahasiswa();
+
+// ARROW FUNCTION (tidal memiliki konsep this)
+// const Mahasiswa = function () {
+//   this.nama = "Taqiyyah";
+//   this.umur = "22";
+//   this.sayHello = function () {
+//     console.log(`Halo, nama saya ${this.nama}, dsn saya ${this.umur} tahun.`);
+//   };
+// };
+// const taqi = new Mahasiswa();
+
+// OBJECT LITERAL
+// const mhs1 = {
+//   nama: "Taqiyyah",
+//   umur: 22,
+//   sayHello: () => {
+//     console.log(`Halo, nama saya ${this.nama}, dsn saya ${this.umur} tahun.`);
+//   },
+// };
+
+const box = document.querySelector(".box");
+box.addEventListener("click", function () {
+  let satu = "size";
+  let dua = "caption";
+
+  if (this.classList.contains(satu)) {
+    [satu, dua] = [dua, satu];
+  }
+
+  this.classList.toggle(satu);
+  setTimeout(() => {
+    this.classList.toggle(dua);
+  }, 600);
+});
