1c6807385c1a11fac12c50c2d3093361b18630ec
taqiyyhdh <taqiyyhdh@gmail.com>
2026-01-17 12:27:06 +0700

    latihan MOVIE DB-add detail

--- lat/script.js
+++ lat/script.js
@@ -4,21 +4,59 @@ $.ajax({
     const movies = results.Search;
     let cards = "";
     movies.forEach((m) => {
-      cards += `
-        <div class="col-md-4 my-5">
-          <div class="card">
-            <img src="${m.Poster}" class="card-img-top" alt="" />
-            <div class="card-body">
-              <h5 class="card-title">${m.Title}</h5>
-              <h6 class="card-subtittle mb-2 text-muted">${m.Year}</h6>
-              <a href="#" class="btn btn-primary">Show Details</a>
-            </div>
-          </div>
-        </div>`;
+      cards += showCards(m);
     });
     $(".movie-container").html(cards);
+
+    // ketika tombol detail di klik
+    $(".modal-detail-button").on("click", function () {
+      $.ajax({
+        url:
+          "http://www.omdbapi.com/?apikey=54a08682&i=" + $(this).data("imdbid"),
+        success: (m) => {
+          const movieDetail = showDetail(m);
+          $(".modal-body").html(movieDetail);
+        },
+        error: (e) => {
+          console.log(e.responseText);
+        },
+      });
+    });
   },
   error: (e) => {
     console.log(e.responseText);
   },
 });
+
+function showCards(m) {
+  return `<div class="col-md-4 my-5">
+          <div class="card">
+            <img src="${m.Poster}" class="card-img-top" alt="" />
+            <div class="card-body">
+              <h5 class="card-title">${m.Title}</h5>
+              <h6 class="card-subtittle mb-2 text-muted">${m.Year}</h6>
+              <a href="#" class="btn btn-primary modal-detail-button" data-bs-toggle="modal" data-bs-target="#movieDetailModal" data-imdbid="${m.imdbID}">Show Details</a>
+            </div>
+          </div>
+        </div>`;
+}
+
+function showDetail(m) {
+  return `<div class="container-fluid">
+              <div class="row">
+                <div class="col-md-3">
+                  <img src="${m.Poster}" class="img-fluid" alt="" />
+                </div>
+                <div class="col-md">
+                  <ul class="list-group">
+                    <li class="list-group-item"><h4>${m.Title} (${m.Year} )</h4></li>
+                    <li class="list-group-item"><strong>Director:</strong> ${m.Director}</li>
+                    <li class="list-group-item"><strong>Actors:</strong> ${m.Actors}</li>
+                    <li class="list-group-item"><strong>Writer:</strong> ${m.Writer}</li>
+                    <li class="list-group-item"><strong>Plot:</strong> ${m.Plot}</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          `;
+}
